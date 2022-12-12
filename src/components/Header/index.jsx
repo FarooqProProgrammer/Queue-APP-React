74f25c318653ec14df6e2ef656856a2b1477642a
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-12 20:12:34 +0500

    Initialize

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+
+function Header() {
+  return (
+    <div className='w-full h-[70px] border-2 border-black'>
+        
+
+    </div>
+  )
+}
+
+export default Header
