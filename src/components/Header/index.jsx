c127acc7581f143f954aa7e2561fe9de4763c128
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-12 21:28:56 +0500

    Commiting

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,14 +7,42 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import {Modal,Input } from 'antd';
 import { GoogleAuthProvider,getAuth ,signInWithPopup} from "firebase/auth";
+import {  FacebookAuthProvider } from "firebase/auth";
 import app from '../../config/Firebase';
 
 function Header() {
 
   const auth  = getAuth(app)
   const provider = new GoogleAuthProvider();
+  const provider_fb = new FacebookAuthProvider();
   provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
+
+  const fb = ()=>{
+    signInWithPopup(auth, provider_fb)
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
+  
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+  
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = FacebookAuthProvider.credentialFromError(error);
+  
+      // ...
+    });
+  }
 
 
   const GoogleLogin = ()=>{
@@ -80,7 +108,7 @@ function Header() {
 
 
         <Button className='w-full' onClick={GoogleLogin}>Login With Google</Button>
-        <Button className='w-full'>Login With Facebook</Button>
+        <Button className='w-full' onClick={fb}>Login With Facebook</Button>
         
         </div>
 
