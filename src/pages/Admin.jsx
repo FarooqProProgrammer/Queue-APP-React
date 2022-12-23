a053a0be45f16f6636baea5681c578fef5b6ed8c
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-23 18:49:14 +0500

    Done It

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -9,156 +9,86 @@ import {
  import {SubmitInfo} from "./AdminConfig/functions"
 import { Footer } from './HomeConfig'
 import { useSelector } from 'react-redux'
-function Admin() {
-
-    const storage = getStorage(app)
-    const db = getFirestore(app)
-    const auth = getAuth(app)
-
-    const user = auth.currentUser;
-    //// console.log(user);
-
-// ===================================================================================================
-    const [company_name,setCompanyName] = useState("");
-    const [company_year,setCompanyYear] = useState("");
-    const [stTime,setTiming] = useState()
-    const [edTime,setEndTime] = useState()
-    const [img,setImage] = useState(null);
-    const [url,setUrl] = useState();
-    const [country,setCountry] = useState([])    
-
-    const Company_Name = (e)=>{
-        setCompanyName(e.target.value)
-        //// console.log(e.target.value);
-    }
-    const Company_year = (e)=>{
-        setCompanyYear(e.target.value)
-    }
-    const start_timing = (e)=>{
-        setTiming(e.target.value)
-    }
-    const End_Time = (e)=>{
-        setEndTime(e.target.value)
-    }
-    const [country_name,setCountryName] = useState()
-    const SubmitInfo =async ()=>{
-      // console.log(user.uid);
-      // console.log(company_name);
-      // console.log(company_year);
-      // console.log(stTime);
-      // console.log(edTime);
-      // console.log(country_name);
-      // console.log(url);
-
-      const docRef = await addDoc(collection(db, "Company"), {
-        user:user.uid,
-        Company_Name: company_name,
-        StartingYear:company_year,
-        Start_timing:stTime,
-        End_Time:edTime,
-        Country:"Pakistan",
-        url:url,
-        Time:Date.now()
-      });
-     
-    }    
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [colorTheme,setTheme] = useState();
-    const showModal = () => {
-      setIsModalOpen(true);
-    };
-    let them = useSelector(state =>state.ThemeReducer.theme)
-      setTheme(them)
-      document.body.style.backgroundColor = them
-    const handleOk = () => {
-      setIsModalOpen(false);
-
-    
-    };
-    const handleCancel = () => {
-      setIsModalOpen(false);
-    };
-
-// =====================================================================================================
-
-// ========================================================================================================
-
-useEffect(()=>{
-      
-      // //// console.log(Country.getAllCountries())
-      setCountry(Country.getAllCountries())
-      //// console.log(State.getAllStates())
-    },[])
 
 
-    const { Search } = Input;
-    const suffix = (
-      <AudioOutlined
-        style={{
-          fontSize: 16,
-          color: '#1890ff',
-        }}
-      />
-    );
+const Admin = () => {
+  // ======================= Variables ====================================
+  const storage = getStorage(app)
+  const db = getFirestore(app)
+  const auth = getAuth(app)
 
-    // success(country)
-    const success = (country) => {
-      Modal.success({
-        content: `${country} is Founded Successfully`,
-      });
-    };
-
+  const user = auth.currentUser;
 
   
-    const onSearch = (value)=>{
-      setCountryName(value)
-        let flag = false
-        for(let i =0;i<country.length;i++){
-            if(country[i].name === value){
-              // //// console.log(i);
-                //// console.log(country[i].name);
-                success(country[i].name)
-            }
-            flag = true
-        }
-
-
-      //// console.log("Search");
-    }
+  // ======================== UseStates ==========================================================
+  const [company_name,setCompanyName] = useState("");
+  const [company_year,setCompanyYear] = useState("");
+  const [stTime,setTiming] = useState()
+  const [edTime,setEndTime] = useState()
+  const [img,setImage] = useState(null);
+  const [url,setUrl] = useState();
+  const [country,setCountry] = useState([])
+  const [country_name,setCountryName] = useState()
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [colorTheme,setTheme] = useState();
 
 
 
-    const handleChange = (event)=>{
+  // ================ States Handle Function ==========================================
+  const Company_Name = (e)=>{
+    setCompanyName(e.target.value)
+      }
+      const Company_year = (e)=>{
+          setCompanyYear(e.target.value)
+      }
+      const start_timing = (e)=>{
+          setTiming(e.target.value)
+      }
+      const End_Time = (e)=>{
+          setEndTime(e.target.value)
+      }
+      const showModal = () => {
+        setIsModalOpen(true);
+      };
+      const handleOk = () => {
+        setIsModalOpen(false);
+  
+      
+      };
+      const handleCancel = () => {
+        setIsModalOpen(false);
+      };
+
+      const success = (country) => {
+        Modal.success({
+          content: `${country} is Founded Successfully`,
+        });
+      };
+      const handleChange = (event)=>{
         setImage(event.target.files[0])
         uploadImage(event.target.files[0])
        
     }
-
-   
-     async function uploadImage(image) {
-    
-      // const storageRef = ref(storage, `images/${image.name}`)
-      // uploadBytes(storageRef,image).then(()=>{
-      //   // console.log("Image Uploaded");
-      // })
-      //   getDownloadURL(ref(storage, storageRef))
-      //   .then((url) => {
-        
-      //       // console.log(url);
-      //       setUrl(url)
-         
-      //   })
-      //   .catch((error) => {
-       
-      //   });
-
-
+    async function uploadImage(image) {
       const storageRef = ref(storage, `images/${image.name}`)
       const snapshot = await uploadBytes(storageRef, image)
       const url = await getDownloadURL(snapshot.ref)
       setUrl(url)
-    }
+    }  
+
+      // let them = useSelector(state =>state.ThemeReducer.theme)
+      // useEffect(()=>{
+      //   document.body.style.backgroundColor = them
+      // },[colorTheme])
+      // setTheme(them)
+     
+
+
+
+
+
   return (
+    
     <div>
         <Header/>
 
