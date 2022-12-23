867e4c5a2c92fb9e0dc40a055522aabc27ca5b29
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-23 09:43:37 +0500

    update

--- src/Re-useable/StopWatch/index.jsx
+++ src/Re-useable/StopWatch/index.jsx
@@ -1,40 +1,9 @@
-import React ,{useEffect, useState}from 'react'
-import ReactStopwatch from 'react-stopwatch';
-function StopWatch() {
+import React from 'react'
 
-
+const index = () => {
   return (
-    <div>
-<ReactStopwatch
-    seconds={0}
-    minutes={0}
-    hours={0}
-    limit="00:00:10"
-    onChange={({ hours, minutes, seconds }) => {
-      // do something
-    }}
-    onCallback={() => console.log('Finish')}
-    render={({ formatted, hours, minutes, seconds }) => {
-      return (
-        <div>
-          <p>
-            Formatted: { formatted }
-          </p>
-          <p>
-            Hours: { hours }
-          </p>
-          <p>
-            Minutes: { minutes }
-          </p>
-          <p>
-            Seconds: { seconds }
-          </p>
-        </div>
-      );
-    }}
-   />
-    </div>
+    <div>index</div>
   )
 }
 
-export default StopWatch
+export default index
